5b459898f3112b8c33aa445d006ba52966406691
Pallavi Taneja <pallavit@users.noreply.github.com>
2022-02-08 14:56:25 -0800

    Reset changes to the patch version.

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareFileClient.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareFileClient.java
@@ -116,6 +116,7 @@ public class ShareFileClient {
 
     /**
      * Opens a file input stream to download the file.
+     * <p>
      *
      * @return An <code>InputStream</code> object that represents the stream to use for reading from the file.
      * @throws ShareStorageException If a storage service error occurred.
@@ -126,6 +127,7 @@ public class ShareFileClient {
 
     /**
      * Opens a file input stream to download the specified range of the file.
+     * <p>
      *
      * @param range {@link ShareFileRange}
      * @return An <code>InputStream</code> object that represents the stream to use for reading from the file.
