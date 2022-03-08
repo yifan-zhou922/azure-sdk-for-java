17183b4c5e9d4cad5af2221cb971ef7fdede7577
Pallavi Taneja <pallavit@users.noreply.github.com>
2022-03-08 12:04:49 -0800

    Reset sources for azure-security-keyvault-certificates to the release version 4.2.7.

--- sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateServiceVersion.java
+++ sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateServiceVersion.java
@@ -22,12 +22,7 @@ public enum CertificateServiceVersion implements ServiceVersion {
     /**
      * Service version {@code 7.2}.
      */
-    V7_2("7.2"),
-
-    /**
-     * Service version {@code 7.3-preview}.
-     */
-    V7_3_PREVIEW("7.3-preview");
+    V7_2("7.2");
 
     private final String version;
 
@@ -49,6 +44,6 @@ public enum CertificateServiceVersion implements ServiceVersion {
      * @return the latest {@link CertificateServiceVersion}
      */
     public static CertificateServiceVersion getLatest() {
-        return V7_3_PREVIEW;
+        return V7_2;
     }
 }
