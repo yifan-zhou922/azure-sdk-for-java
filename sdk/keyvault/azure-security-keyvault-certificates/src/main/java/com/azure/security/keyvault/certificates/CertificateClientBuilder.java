e886e3e935f43bad3994cff364c9882ac7056026
Pallavi Taneja <pallavit@users.noreply.github.com>
2022-03-08 11:17:26 -0800

    Reset sources for azure-security-keyvault-certificates to the release version 4.2.7.

--- sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateClientBuilder.java
+++ sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateClientBuilder.java
@@ -4,9 +4,6 @@
 package com.azure.security.keyvault.certificates;
 
 import com.azure.core.annotation.ServiceClientBuilder;
-import com.azure.core.client.traits.ConfigurationTrait;
-import com.azure.core.client.traits.HttpTrait;
-import com.azure.core.client.traits.TokenCredentialTrait;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.HttpHeader;
@@ -20,14 +17,11 @@ import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.http.policy.HttpLoggingPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.HttpPolicyProviders;
-import com.azure.core.http.policy.RetryOptions;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.util.ClientOptions;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
-import com.azure.core.util.HttpClientOptions;
-import com.azure.core.util.builder.ClientBuilderUtil;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.security.keyvault.certificates.implementation.KeyVaultCredentialPolicy;
 import com.azure.security.keyvault.certificates.models.KeyVaultCertificateIdentifier;
@@ -108,10 +102,7 @@ import java.util.Map;
  * @see CertificateClient
  */
 @ServiceClientBuilder(serviceClients = {CertificateClient.class, CertificateAsyncClient.class})
-public final class CertificateClientBuilder implements
-    TokenCredentialTrait<CertificateClientBuilder>,
-    HttpTrait<CertificateClientBuilder>,
-    ConfigurationTrait<CertificateClientBuilder> {
+public final class CertificateClientBuilder {
     private final ClientLogger logger = new ClientLogger(CertificateClientBuilder.class);
     // This is properties file's name.
     private static final String AZURE_KEY_VAULT_CERTIFICATES_PROPERTIES = "azure-key-vault-certificates.properties";
@@ -128,7 +119,6 @@ public final class CertificateClientBuilder implements
     private HttpClient httpClient;
     private HttpLogOptions httpLogOptions;
     private RetryPolicy retryPolicy;
-    private RetryOptions retryOptions;
     private Configuration configuration;
     private CertificateServiceVersion version;
     private ClientOptions clientOptions;
@@ -137,6 +127,7 @@ public final class CertificateClientBuilder implements
      * The constructor with defaults.
      */
     public CertificateClientBuilder() {
+        retryPolicy = new RetryPolicy();
         httpLogOptions = new HttpLogOptions();
         perCallPolicies = new ArrayList<>();
         perRetryPolicies = new ArrayList<>();
@@ -158,8 +149,6 @@ public final class CertificateClientBuilder implements
      *
      * @throws IllegalStateException If {@link CertificateClientBuilder#credential(TokenCredential)} or
      * {@link CertificateClientBuilder#vaultUrl(String)} have not been set.
-     * @throws IllegalStateException If both {@link #retryOptions(RetryOptions)}
-     * and {@link #retryPolicy(RetryPolicy)} have been set.
      */
     public CertificateClient buildClient() {
         return new CertificateClient(buildAsyncClient());
@@ -180,8 +169,6 @@ public final class CertificateClientBuilder implements
      *
      * @throws IllegalStateException If {@link CertificateClientBuilder#credential(TokenCredential)} or {@link
      * CertificateClientBuilder#vaultUrl(String)} have not been set.
-     * @throws IllegalStateException If both {@link #retryOptions(RetryOptions)}
-     * and {@link #retryPolicy(RetryPolicy)} have been set.
      */
     public CertificateAsyncClient buildAsyncClient() {
         Configuration buildConfiguration = (configuration != null) ? configuration
@@ -228,7 +215,7 @@ public final class CertificateClientBuilder implements
         HttpPolicyProviders.addBeforeRetryPolicies(policies);
 
         // Add retry policy.
-        policies.add(ClientBuilderUtil.validateAndGetRetryPolicy(retryPolicy, retryOptions));
+        policies.add(retryPolicy == null ? new RetryPolicy() : retryPolicy);
 
         policies.add(new KeyVaultCredentialPolicy(credential));
 
@@ -272,18 +259,15 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Sets the {@link TokenCredential} used to authorize requests sent to the service. Refer to the Azure SDK for Java
-     * <a href="https://aka.ms/azsdk/java/docs/identity">identity and authentication</a>
-     * documentation for more details on proper usage of the {@link TokenCredential} type.
+     * Sets the credential to use when authenticating HTTP requests.
      *
-     * @param credential {@link TokenCredential} used to authorize requests sent to the service.
+     * @param credential The credential to use for authenticating HTTP requests.
      *
      * @return The updated {@link CertificateClientBuilder} object.
      *
      * @throws NullPointerException If {@code credential} is {@code null}.
      *
      */
-    @Override
     public CertificateClientBuilder credential(TokenCredential credential) {
         if (credential == null) {
             throw logger.logExceptionAsError(new NullPointerException("'credential' cannot be null."));
@@ -295,21 +279,14 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Sets the {@link HttpLogOptions logging configuration} to use when sending and receiving requests to and from
-     * the service. If a {@code logLevel} is not provided, default value of {@link HttpLogDetailLevel#NONE} is set.
+     * Sets the logging configuration for HTTP requests and responses.
      *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
+     * <p> If logLevel is not provided, default value of {@link HttpLogDetailLevel#NONE} is set.</p>
      *
-     * @param logOptions The {@link HttpLogOptions logging configuration} to use when sending and receiving requests to
-     * and from the service.
+     * @param logOptions The logging configuration to use when sending and receiving HTTP requests/responses.
+     *
      * @return The updated {@link CertificateClientBuilder} object.
      */
-    @Override
     public CertificateClientBuilder httpLogOptions(HttpLogOptions logOptions) {
         httpLogOptions = logOptions;
 
@@ -317,21 +294,15 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Adds a {@link HttpPipelinePolicy pipeline policy} to apply on each request sent.
+     * Adds a policy to the set of existing policies that are executed after {@link CertificateAsyncClient} and {@link
+     * CertificateClient} required policies.
      *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
+     * @param policy The {@link HttpPipelinePolicy policy} to be added.
      *
-     * @param policy A {@link HttpPipelinePolicy pipeline policy}.
      * @return The updated {@link CertificateClientBuilder} object.
      *
      * @throws NullPointerException If {@code policy} is {@code null}.
      */
-    @Override
     public CertificateClientBuilder addPolicy(HttpPipelinePolicy policy) {
         if (policy == null) {
             throw logger.logExceptionAsError(new NullPointerException("'policy' cannot be null."));
@@ -347,19 +318,12 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Sets the {@link HttpClient} to use for sending and receiving requests to and from the service.
+     * Sets the HTTP client to use for sending and receiving requests to and from the service.
      *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
+     * @param client The HTTP client to use for requests.
      *
-     * @param client The {@link HttpClient} to use for requests.
      * @return The updated {@link CertificateClientBuilder} object.
      */
-    @Override
     public CertificateClientBuilder httpClient(HttpClient client) {
         this.httpClient = client;
 
@@ -367,22 +331,16 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Sets the {@link HttpPipeline} to use for the service client.
+     * Sets the HTTP pipeline to use for the service client.
      *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
-     * <p>
-     * The {@link #vaultUrl(String) vaultUrl} is not ignored when
-     * {@code pipeline} is set.
+     * If {@code pipeline} is set, all other settings are ignored, aside from
+     * {@link CertificateClientBuilder#vaultUrl(String) vaultUrl} to build {@link CertificateClient} or {@link
+     * CertificateAsyncClient}.
      *
-     * @param pipeline {@link HttpPipeline} to use for sending service requests and receiving responses.
+     * @param pipeline The HTTP pipeline to use for sending service requests and receiving responses.
+     *
      * @return The updated {@link CertificateClientBuilder} object.
      */
-    @Override
     public CertificateClientBuilder pipeline(HttpPipeline pipeline) {
         this.pipeline = pipeline;
 
@@ -399,7 +357,6 @@ public final class CertificateClientBuilder implements
      *
      * @return The updated {@link CertificateClientBuilder} object.
      */
-    @Override
     public CertificateClientBuilder configuration(Configuration configuration) {
         this.configuration = configuration;
 
@@ -428,8 +385,6 @@ public final class CertificateClientBuilder implements
      *
      * The default retry policy will be used in the pipeline, if not provided.
      *
-     * Setting this is mutually exclusive with using {@link #retryOptions(RetryOptions)}.
-     *
      * @param retryPolicy user's retry policy applied to each request.
      *
      * @return The updated {@link CertificateClientBuilder} object.
@@ -441,45 +396,17 @@ public final class CertificateClientBuilder implements
     }
 
     /**
-     * Sets the {@link RetryOptions} for all the requests made through the client.
+     * Sets the {@link ClientOptions} which enables various options to be set on the client. For example setting an
+     * {@code applicationId} using {@link ClientOptions#setApplicationId(String)} to configure
+     * the {@link UserAgentPolicy} for telemetry/monitoring purposes.
      *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
-     * <p>
-     * Setting this is mutually exclusive with using {@link #retryPolicy(RetryPolicy)}.
+     * <p>More About <a href="https://azure.github.io/azure-sdk/general_azurecore.html#telemetry-policy">Azure Core:
+     * Telemetry policy</a>
      *
-     * @param retryOptions The {@link RetryOptions} to use for all the requests made through the client.
+     * @param clientOptions the {@link ClientOptions} to be set on the client.
+     *
      * @return The updated {@link CertificateClientBuilder} object.
      */
-    @Override
-    public CertificateClientBuilder retryOptions(RetryOptions retryOptions) {
-        this.retryOptions = retryOptions;
-        return this;
-    }
-
-    /**
-     * Allows for setting common properties such as application ID, headers, proxy configuration, etc. Note that it is
-     * recommended that this method be called with an instance of the {@link HttpClientOptions}
-     * class (a subclass of the {@link ClientOptions} base class). The HttpClientOptions subclass provides more
-     * configuration options suitable for HTTP clients, which is applicable for any class that implements this HttpTrait
-     * interface.
-     *
-     * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
-     * particular, if a {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
-     * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
-     * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
-     * documentation of types that implement this trait to understand the full set of implications.</p>
-     *
-     * @param clientOptions A configured instance of {@link HttpClientOptions}.
-     * @see HttpClientOptions
-     * @return The updated {@link CertificateClientBuilder} object.
-     */
-    @Override
     public CertificateClientBuilder clientOptions(ClientOptions clientOptions) {
         this.clientOptions = clientOptions;
 
